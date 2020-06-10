adf0d687731d1c8d3d06d96ad608b63209985047
lucawynters <64028925+lucawynters@users.noreply.github.com>
2020-06-10 14:22:13 +0200

    Add files via upload

--- havaj.js
+++ havaj.js
@@ -7,29 +7,15 @@ let osoba2 = {
   uspory: 68000
 };
 
-// - zda vůbec mohou nebo nemohou na dovolenou jet
-let savings = osoba1.uspory + osoba2.uspory;
-
-const holidaysYet = (savings) => {
-  if (savings >= 100000) {
-    console.log('Můžete vyrazit')
-  } else {
-    console.log('Ještě šetřete')
-  }
-};
-// - pokud mohou jet (tj. mají dohromady 100 tisíc):
-//   - napiš, zda obě mají naspořeno dost (každá alespoň 50 tisíc)
-//   - nebo pokud má jedna kamarádka méně, tak napiš její jméno a kolik jí
-//     ještě chybí do 50 tisíc a tudíž kolik bude muset po dovolené splatit
-
 const holidaysTogether = (osoba1, osoba2) => {
-  if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
+  let savings = osoba1.uspory + osoba2.uspory;
+  if (savings >= 100000) {
     console.log('Můžete vyrazit')
   } else if (osoba1.uspory >= 50000 && osoba2.uspory <= 50000 && osoba1.uspory + osoba2.uspory >= 100000) {
     console.log(`Karolína bude muset doplatit ${50000 - osoba2.uspory}`)
   } else if (osoba1.uspory <= 50000 && osoba2.uspory >= 50000 && osoba1.uspory + osoba2.uspory >= 100000) {
     console.log(`Alena bude muset doplatit ${50000 - osoba1.uspory}`)
-  } else {
-    console.log('Ještě šetřete')
-  }
+  } else if (osoba1.uspory <= 50000 && osoba2.uspory <= 50000) { console.log(`Ještě vám chybí ${100000 - savings}`) + console.log(`${osoba1.jmeno} bude muset doplatit ${50000 - osoba1.uspory} a ${osoba2.jmeno} bude muset doplatit ${50000 - osoba2.uspory}`) }
 };
+
+holidaysTogether(osoba1, osoba2)
